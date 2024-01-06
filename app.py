33c7b294193b62ba4c11ab474ba8384efb2d6a3a
kitindi <abdullkitindi@gmail.com>
2024-01-06 12:36:06 +0300

    added job description page

--- app.py
+++ app.py
@@ -2,9 +2,44 @@ from flask import Flask, render_template
 
 app= Flask(__name__)
 
+JOBS = [
+    {"id": 1,
+     "title": "Data Analyst",
+     "level":"Entry Level",
+     "Location": "Dar es Salaam, Tanzania",
+     "Salary": "1,500,000",
+     "Mode":["Full-time","Hybrid"]
+     },
+    {"id": 2,
+     "title": "Frontend Developer",
+     "level":"Entry Level",
+     "Location": "Tabora, Tanzania",
+     "Mode":["Full-time","Office"]
+     },
+    {"id": 3,
+     "title": "UI/UX designer",
+     "level":"Entry Level",
+     "Location": "Arusha, Tanzania",
+     "Salary": "1,400,000",
+     "Mode":["Full-time","Remote"]
+     },
+    {"id": 4,
+     "title": "Backend Engineer",
+     "level":"Entry Level",
+     "Location": "Arusha, Tanzania",
+     "Salary": "1,400,000",
+     "Mode":["Full-time","Hybrid"]
+     },
+    ]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+@app.route('/job')
+def job_details():
+    return render_template('job_detail.html')
+    
 
 
 """
